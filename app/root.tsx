0d3a904e8deccf5d4d103013b3f687e702c80a81
zerodice0 <zerodice0@gmail.com>
2022-01-13 01:29:34 +0900

    - remove testing function 'addA' - add Scripts to body. - add Meta to body.

--- app/root.tsx
+++ app/root.tsx
@@ -40,10 +40,7 @@ declare global {
 }
 
 function Map() {
-  const [a, setA] = useState<number>(0);
-
   useEffect(() => {
-    console.log("hello");
     let container = document.getElementById('map');
     let options = {
       center: new window.kakao.maps.LatLng(33.450701, 126.570667),
@@ -58,12 +55,8 @@ function Map() {
   }
 
   return (
-    <>
-      <div>{a}</div>
-      <button onClick={addA}>+</button>
-      <div id="map" style={{width: "100vw", height: "100vh"}}></div>
-    </>
-  );
+    <div id="map" style={{width: "100vw", height: "100vh"}}></div>
+  )
 }
 
 function Document({
@@ -78,12 +71,14 @@ function Document({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        {title ? <title>{title}</title> : null}
         <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9c00a28773c06d77e17b4fd10f8fa42c"></script>
+        <title>TeaMory</title>
       </head>
       <body>
         {children}
         {process.env.NODE_ENV === "development" && <LiveReload />}
+        <Meta/>
+        <Scripts/>
       </body>
     </html>
   );
