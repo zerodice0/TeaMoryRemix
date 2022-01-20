68af18f5929c43d209d65f1f99b3d16c11b8d67f
zerodice0 <zerodice0@gmail.com>
2022-01-20 23:38:05 +0900

    - add default value to latitude, longitude.

--- app/modules/location/useCurrentLocation.tsx
+++ app/modules/location/useCurrentLocation.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
+import { GeoLocation } from "./model/model";
 
 const useCurrentLocation = () => {
-  const [location, setLocation] = useState<GeolocationPosition>();
+  const [location, setLocation] = useState<GeoLocation>(
+    {
+      latitude: 33.450701,
+      longitude: 126.570667
+    }
+  );
   const [error, setError] = useState<Error>();
 
   useEffect(() => {
     if (navigator && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLocation(position);
+          const location: GeoLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }
+          setLocation(location);
         },
         (geolocationPositionError) => {
           setError(
