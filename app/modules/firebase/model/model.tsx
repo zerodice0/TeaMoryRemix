c202983cf01601ee231d9df49398041c7d69cec2
zerodice0 <zerodice0@gmail.com>
2022-01-25 01:27:02 +0900

    add useFirebase custom hooks.

---
+++ app/modules/firebase/model/model.tsx
@@ -0,0 +1,9 @@
+interface FirebaseConfig {
+  apiKey: string | undefined,
+  authDomain: string | undefined,
+  projectId: string | undefined,
+  storageBucket: string | undefined,
+  messagingSenderId: string | undefined,
+  appId: string | undefined,
+  measurementId: string | undefined
+}
---
+++ app/modules/firebase/useFirebase.tsx
@@ -0,0 +1,42 @@
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { collection, DocumentData, Firestore, getDocs, getFirestore, QuerySnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "remix"
+
+const useFirebase = () => {
+  const {firebaseConfig} = useLoaderData();
+
+  const [app, setApp] = useState<FirebaseApp | null>(null);
+  const [database, setDatabase] = useState<Firestore | null>(null);
+  const [analytics, setAnalytics] = useState<Analytics | null>(null);
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const database = getFirestore(app);
+    const analytics = getAnalytics(app);
+
+    setApp(app);
+    setDatabase(database);
+    setAnalytics(analytics);
+  }, [])
+
+  const getCollection = async (path: string) => {
+    let result: QuerySnapshot<DocumentData> | null = null;
+    
+    if (database != null) {
+      result = await getDocs(collection(database, path));
+    }
+
+    return result;
+  }
+
+  return {
+    app,
+    database,
+    analytics,
+    getCollection
+  };
+}
+
+export default useFirebase;
